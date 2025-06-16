64d2f44d7ce7507dd7b76278c3172ee4230b7930
PoohCook <PoohCook@gmail.com>
2025-06-16 17:47:40 -0600

    Added filtering to inventory

--- modules/inv/inventory.py
+++ modules/inv/inventory.py
@@ -1,18 +1,28 @@
+from ..cars import Car
 
 
 class Inventory:
     def __init__(self):
         self.__cars = []
 
-    def add(self, car):
+    def add(self, car: Car):
         if car in self.__cars:
             raise ValueError("Car already exists in inventory.")
         self.__cars.append(car)
 
-    def remove(self, car):
+    def remove(self, car: Car):
         if car not in self.__cars:
             raise ValueError("Car not found in inventory.")
         self.__cars.remove(car)
 
-    def get(self):
-        return self.__cars.copy()
+    def query(self, make: str = None, model: str = None, year: int = None):
+        cars = self.__cars.copy()
+
+        if make:
+            cars = [c for c in cars if c.manufacturer().name() == make]
+        if model:
+            cars = [c for c in cars if c.model() == model]
+        if year:
+            cars = [c for c in cars if c.year() == year]
+
+        return cars
--- test/InvTest.py
+++ test/InvTest.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import unittest
 from modules.cars import Car, Manufacturer, BMW, Tesla
 from modules.inv import Inventory
@@ -24,7 +25,7 @@ class InvTest(unittest.TestCase):
     def testCreateInventory(self):
         print("----------- testCreateInventory  ------------")
 
-        inv = self.inventory.get()
+        inv = self.inventory.query()
         self.assertEqual(len(inv), 3)
         self.assertEqual(inv[0].manufacturer().name(), "Toyota")
         self.assertEqual(inv[1].manufacturer().name(), "BMW")
@@ -36,7 +37,7 @@ class InvTest(unittest.TestCase):
     def testCreateInventoryDuplicate(self):
         print("----------- testCreateInventoryDuplicate  ---")
 
-        car = self.inventory.get()[0]
+        car = self.inventory.query()[0]
 
         # test duplicate object
         with self.assertRaises(ValueError) as context:
@@ -51,15 +52,15 @@ class InvTest(unittest.TestCase):
             self.inventory.add(car)
         self.assertEqual('Car already exists in inventory.', str(context.exception))
 
-        inv = self.inventory.get()
+        inv = self.inventory.query()
         self.assertEqual(len(inv), 3)
 
     def testRemoveInventoryDuplicate(self):
         print("----------- testRemoveInventoryDuplicate  ---")
 
-        car = self.inventory.get()[0]
+        car = self.inventory.query()[0]
         self.inventory.remove(car)
-        inv = self.inventory.get()
+        inv = self.inventory.query()
         self.assertEqual(len(inv), 2)
         self.assertEqual(inv[0].manufacturer().name(), "BMW")
         self.assertEqual(inv[1].manufacturer().name(), "Tesla")
@@ -69,7 +70,7 @@ class InvTest(unittest.TestCase):
         bmw = BMW()
         car = Car(bmw, "WBA8E1C5XJY123456", "X5", 2023, "Blue")
         self.inventory.remove(car)
-        inv = self.inventory.get()
+        inv = self.inventory.query()
         self.assertEqual(len(inv), 1)
         self.assertEqual(inv[0].manufacturer().name(), "Tesla")
         self.assertEqual(inv[0].vehicleIdNumber(), "5YJ3E1EA7JF123456")
@@ -77,3 +78,63 @@ class InvTest(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             self.inventory.remove(car)
         self.assertEqual('Car not found in inventory.', str(context.exception))
+
+    def testQueryInventory(self):
+        print("----------- testQueryInventory  -------------")
+        toyo = Manufacturer("Toyota", "Japan")
+        car = Car(toyo, "5TDKZRBCMRS234561", "Camary", 2024, "Green")
+        self.inventory.add(car)
+        car = Car(toyo, "5TDKZRBCMRS345612", "Camary", 2023, "Yellow")
+        self.inventory.add(car)
+        car = Car(toyo, "5TDKZRBCMRS345612", "SR5", 2023, "Silver")
+        self.inventory.add(car)
+
+        bmw = BMW()
+        car = Car(bmw, "WBA8E1C3XJY234561", "X3", 2023, "Silver")
+        self.inventory.add(car)
+        car = Car(bmw, "WBA8E1C3XJY345612", "X3", 2023, "Black")
+        self.inventory.add(car)
+        car = Car(bmw, "WBA8E1C5XJY345612", "X5", 2023, "Red")
+        self.inventory.add(car)
+
+        tesla = Tesla()
+        car = Car(tesla, "5YJ3E1EA6JF123456", "Model S", 2023, "Black")
+        self.inventory.add(car)
+        car = Car(tesla, "5YJ3E1EA7JF234561", "Model S", 2023, "White")
+        self.inventory.add(car)
+        car = Car(tesla, "5YJ3E1EA7JF345612", "Model S", 2021, "Black")
+        self.inventory.add(car)
+
+        inv = self.inventory.query()
+        self.assertEqual(len(inv), 12)
+
+        inv = sorted(self.inventory.query(year=2023), key=lambda c: c.manufacturer().name())
+        expected = [
+            'Blue, 2023, BMW, X5, WBA8E1C5XJY123456 © BMW AG, Munich, Germany',
+            'Silver, 2023, BMW, X3, WBA8E1C3XJY234561 © BMW AG, Munich, Germany',
+            'Black, 2023, BMW, X3, WBA8E1C3XJY345612 © BMW AG, Munich, Germany',
+            'Red, 2023, BMW, X5, WBA8E1C5XJY345612 © BMW AG, Munich, Germany',
+            'Black, 2023, Tesla, Model S, 5YJ3E1EA6JF123456 (Batteries Included!)',
+            'White, 2023, Tesla, Model S, 5YJ3E1EA7JF234561 (Batteries Included!)',
+            'Yellow, 2023, Toyota, Camary, 5TDKZRBCMRS345612',
+            'Silver, 2023, Toyota, SR5, 5TDKZRBCMRS345612',
+        ]
+        self.assertEqual([str(c) for c in inv], expected)
+
+        inv = sorted(self.inventory.query(make="BMW"), key=lambda c: c.year())
+        expected = [
+            'Blue, 2023, BMW, X5, WBA8E1C5XJY123456 © BMW AG, Munich, Germany',
+            'Silver, 2023, BMW, X3, WBA8E1C3XJY234561 © BMW AG, Munich, Germany',
+            'Black, 2023, BMW, X3, WBA8E1C3XJY345612 © BMW AG, Munich, Germany',
+            'Red, 2023, BMW, X5, WBA8E1C5XJY345612 © BMW AG, Munich, Germany'
+        ]
+        self.assertEqual([str(c) for c in inv], expected)
+
+        inv = sorted(self.inventory.query(model='Model S'), key=lambda c: c.year())
+        expected = [
+            'Black, 2021, Tesla, Model S, 5YJ3E1EA7JF345612 (Batteries Included!)',
+            'White, 2022, Tesla, Model S, 5YJ3E1EA7JF123456 (Batteries Included!)',
+            'Black, 2023, Tesla, Model S, 5YJ3E1EA6JF123456 (Batteries Included!)',
+            'White, 2023, Tesla, Model S, 5YJ3E1EA7JF234561 (Batteries Included!)'
+        ]
+        self.assertEqual([str(c) for c in inv], expected)
